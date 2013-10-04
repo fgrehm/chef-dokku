0ccc70c65cc82c1dfdd9b32011ee72581b46c8c3
Stafford Brunk <wingrunr21@gmail.com>
2013-10-04 15:04:24 -0600

    Create a spec_helper in prep for new recipes

---
+++ spec/spec_helper.rb
@@ -0,0 +1,15 @@
+require 'chefspec'
+require 'berkshelf'
+
+berksfile = Berkshelf::Berksfile.from_file('Berksfile')
+berksfile.install(path: 'vendor/cookbooks')
+
+# Define matchers for the docker LWRPs
+# Kill this off in ChefSpec v3 since chef-docker should ship with
+# its own matchers
+
+module ChefSpec
+  module Matchers
+    define_resource_matchers([:pull, :build, :remove, :import], [:docker_image], :name)
+  end
+end
