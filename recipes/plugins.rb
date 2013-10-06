75c38ab64b2c28e5c016be7a7e5d26195f63d680
Stafford Brunk <wingrunr21@gmail.com>
2013-10-06 13:15:49 -0600

    Update plugins recipe to override the nginx-vhosts install hook

--- recipes/plugins.rb
+++ recipes/plugins.rb
@@ -5,6 +5,30 @@ node['dokku']['plugins'].each do |plugin_name, repo_url|
   end
 end
 
+# The nginx install script does some stuff we don't want
+# nuke it and do the install manually
+file "#{node['dokku']['plugins_dir']}/nginx-vhosts/install" do
+  action :delete
+end
+
+template "/etc/init/nginx-reloader.conf" do
+  source "plugins/nginx-vhosts/nginx-reloader.conf"
+  action :create_if_missing
+  owner 'root'
+  group 'root'
+end
+
+template "/etc/nginx/conf.d/dokku.conf" do
+  source "plugins/nginx-vhosts/dokku.conf"
+  action :create_if_missing
+  owner 'root'
+  group 'root'
+end
+
+service "nginx-reloader" do
+  action :start
+end
+
 bash "dokku_plugins_install" do
   cwd node['dokku']['plugins_dir']
   code <<-EOH
