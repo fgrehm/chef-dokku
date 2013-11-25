47db8d9ca6214cee7425276da3732ef837b9c41d
Stafford Brunk <wingrunr21@gmail.com>
2013-11-25 18:19:13 -0500

    Make cookbook version 0.2.0 to match dokku's version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'fgrehm@gmail.com'
 license          'MIT'
 description      'Installs/Configures dokku'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.0.0'
+version          '0.2.0'
 
 supports 'ubuntu', '= 13.04'
 
