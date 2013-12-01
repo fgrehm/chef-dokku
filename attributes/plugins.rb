e89fbe4f78bfb5792a776fbac3a4a4e7d71905fe
Stafford Brunk <wingrunr21@gmail.com>
2013-12-01 16:29:25 -0700

    Make sure sudoers.d is enabled

--- attributes/plugins.rb
+++ attributes/plugins.rb
@@ -3,3 +3,6 @@ default['dokku']['plugin_path'] = '/var/lib/dokku/plugins'
 
 #  Hash, key is plugin name, value is git repo URL
 default['dokku']['plugins'] = {}
+
+# Needed for the nginx reload script
+force_default['authorization']['sudo']['include_sudoers_d'] = true
