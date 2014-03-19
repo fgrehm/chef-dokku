67dd272b80805af03312deafca5e5e982f56176c
André Aizim Kelmanson <akelmanson@gmail.com>
2014-03-19 12:49:50 -0300

    Compatibility with lastest chef-docker version

--- recipes/buildstack.rb
+++ recipes/buildstack.rb
@@ -1,12 +1,12 @@
 # Docker build stack
 if node['dokku']['buildstack']['use_prebuilt']
   docker_image node['dokku']['buildstack']['image_name'] do
-    image_url node['dokku']['buildstack']['prebuilt_url']
+    source node['dokku']['buildstack']['prebuilt_url']
     action :import
   end
 else
   docker_image node['dokku']['buildstack']['image_name'] do
-    image_url node['dokku']['buildstack']['stack_url']
+    source node['dokku']['buildstack']['stack_url']
     action :build
   end
 end
